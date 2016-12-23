9ecbfd9c4b7f25ca5c2b0e6121100f52c17aee0b
Conor Horan-Kates <conor.code@gmail.com>
2016-12-23 09:36:19 -0800

    adding another contrived example

---
+++ examples/factorial.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+## factorial.rb
+
+require_relative File.expand_path(sprintf('%s/../lib/bnchmrkr', File.dirname(__FILE__)))
+
+target = 50_000
+
+tester = Bnchmrkr.new({
+  :recursive => lambda { (1..target).inject(:*) },
+  :iterative => lambda {
+    bases = Hash.new
+    (1..target).collect do |i|
+      bases[i] = i * (i - 1) || 1
+    end
+  },
+})
+
+tester.benchmark!
+
+puts tester
